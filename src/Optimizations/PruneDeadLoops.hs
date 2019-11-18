8190f69182081312a1a888fc27db65e1ef2f03ed
Axel Ulmestig <axel.ulmestig@gmail.com>
2019-11-18 12:00:57 +0100

    Prune dead for loops

--- src/Optimizations/PruneDeadLoops.hs
+++ src/Optimizations/PruneDeadLoops.hs
@@ -6,8 +6,9 @@ import Brainfuck    (Operation(..), Brainfuck)
 pruneDeadLoops = f
 
 f :: Brainfuck -> Brainfuck
-f ((SetValue 0):(Loop _ _):bf) = f ((SetValue 0):bf)
-f ((Loop id bf'):bf)           = Loop id (f bf') : f bf
-f (op:bf)                      = op : f bf
-f []                           = []
+f ((SetValue 0):(Loop _ _):bf)      = f ((SetValue 0):bf)
+f ((SetValue 0):(AddMult _ _ _):bf) = f ((SetValue 0):bf)
+f ((Loop id bf'):bf)                = Loop id (f bf') : f bf
+f (op:bf)                           = op : f bf
+f []                                = []
 
