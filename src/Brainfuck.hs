44a7244618d50a229a5f58d4031edd1d31978a27
Axel Ulmestig <axel.ulmestig@gmail.com>
2019-11-18 10:45:27 +0100

    Slight refactor of Brainfuck definition

--- src/Brainfuck.hs
+++ src/Brainfuck.hs
@@ -4,12 +4,14 @@ module Brainfuck (Operation(..), Brainfuck) where
 import Data.Word8
 
 type LoopId = Int
+type RelativeAddress = Int
+type Factor = Int
 
 data Operation
     = IncrementPointer Int
     | IncrementValue Int
     | SetValue Int
-    | AddMult Int Int --relative address, factor
+    | AddMult RelativeAddress Factor
     | OutputValue
     | ReadValue
     | Loop LoopId [Operation]
