33a82abb888468d46e5b3616f69deba9797d2ced
Axel Ulmestig <axel.ulmestig@gmail.com>
2019-11-19 11:55:20 +0100

    Fix for loop optimization

--- src/Optimizations/ForLoops.hs
+++ src/Optimizations/ForLoops.hs
@@ -1,4 +1,3 @@
-
 module Optimizations.ForLoops (optimizeForLoops) where
 
 import Control.Monad.State.Lazy (runState, State, state)
@@ -12,39 +11,41 @@ optimizeForLoops bf = bf >>= optimizeForLoop
 
 optimizeForLoop :: Operation -> Brainfuck
 optimizeForLoop (Loop loopId bf) =
-    if isValid bf
-    then
-        let
-            loopIds                       = map (((show loopId)++) . ('_':) . show) [1 ..]
-            operations                    = zipWith
-                                              ($)
-                                              (map (uncurry . AddMult) loopIds)
-                                              (toList $ loopIncrements bf)
-        in
-            operations ++ [SetValue 0]
-    else
+  if isValid bf
+  then
+    let
+      loopIds     = map ((show loopId ++) . ('_':) . show) [1 ..]
+      operations  = zipWith
+                      ($)
+                      (map (uncurry . AddMult) loopIds)
+                      (toList $ delete 0 $ loopIncrements bf)
+    in
+      operations ++ [SetValue 0]
+  else
         [Loop loopId (optimizeForLoops bf)]
 optimizeForLoop op = [op]
 
 isValid :: Brainfuck -> Bool
 isValid bf =
-    let
-        f (IncrementPointer n)  = Just n
-        f (IncrementValue _)    = Just 0
-        f _                     = Nothing
-        loopComesBack           = maybe False (==0) $ sum <$> traverse f bf
-        decrementsByOne         = maybe False (==(-1)) $ lookup 0 $ loopIncrements bf
-    in
-        decrementsByOne && loopComesBack
+  let
+    f x = case x of
+      (IncrementPointer n) -> Just n
+      (IncrementValue _)   -> Just 0
+      _                    -> Nothing
+
+    loopComesBack   = (== Just 0) $ sum <$> traverse f bf
+    decrementsByOne = (== Just (-1)) $ lookup 0 $ loopIncrements bf
+  in
+    decrementsByOne && loopComesBack
 
 loopIncrements :: Brainfuck -> Map Int Int
 loopIncrements bf =
-    let
-        updatesListS    = traverse loopIncrementsInternal bf            :: State Int [Map Int Int]
-        updatesS        = foldr (unionWith (+)) empty <$> updatesListS  :: State Int (Map Int Int)
-        (updates, _)    = runState updatesS 0                           :: (Map Int Int, Int)
-    in
-        delete 0 updates -- prevent loop from touching index variable
+  let
+    updatesListS    = traverse loopIncrementsInternal bf            :: State Int [Map Int Int]
+    updatesS        = foldr (unionWith (+)) empty <$> updatesListS  :: State Int (Map Int Int)
+    (updates, _)    = runState updatesS 0                           :: (Map Int Int, Int)
+  in
+    updates
 
 loopIncrementsInternal :: Operation -> State Int (Map Int Int)
 loopIncrementsInternal (IncrementValue n)    = state $ \pOffset -> (singleton pOffset n, pOffset)
