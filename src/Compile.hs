fd456bcec47f164520cf7b2da70bcb918e93faf4
Axel Ulmestig <axel.ulmestig@gmail.com>
2020-03-01 18:59:20 +0100

    Add stdin option to compiler

--- src/Compile.hs
+++ src/Compile.hs
@@ -4,16 +4,16 @@ module Compile (
   compileArgsParser
 ) where
 
-import           Prelude               hiding (readFile)
+import           Prelude               hiding (getContents, readFile)
 
-import           Control.Applicative   (optional, (<**>))
+import           Control.Applicative   (optional, (<**>), (<|>))
 import           Data.Maybe            (fromMaybe)
 import           Data.Semigroup        ((<>))
-import           Data.Text.IO          (readFile)
-import           Options.Applicative   (Parser, action, argument, auto,
-                                        completeWith, help, helper, long,
-                                        metavar, option, short, str, switch,
-                                        value)
+import           Data.Text.IO          (getContents, readFile)
+import           Options.Applicative   (Parser, action, argument, auto, command,
+                                        completeWith, help, helper, info, long,
+                                        metavar, option, progDesc, short, str,
+                                        subparser, switch, value)
 import           System.Exit           (die)
 import           System.FilePath.Posix (takeBaseName)
 import           System.Info           (os)
@@ -29,23 +29,40 @@ data CompileArgs = CompileArgs
     debug         :: Bool,
     optimizations :: OptimizationLevel,
     outfile       :: Maybe String,
-    file          :: String
+    input         :: InputArgs
   }
   deriving (Show, Read)
 
+data InputArgs =
+    File String |
+    Stdin
+  deriving (Show, Read)
+
 compile :: CompileArgs -> IO ()
-compile (CompileArgs debug optLevel mOutfile filePath) = do
-  instructions <- readFile filePath
+compile (CompileArgs debug optLevel mOutfile input) = do
+  instructions <- instructionsIO
   case runP pBrainfuck 0 filePath instructions of
     Left err    -> print err
     Right ops   -> do
-      let programName = fromMaybe (takeBaseName filePath) mOutfile
+      let programName = fromMaybe outfile mOutfile
       let instructions = optimize optLevel ops
       let input = CompileInput debug programName instructions
 
       case os of
         "linux"     -> Linux.compile input
         unsupported -> die $ "Error: unsupported os: " ++ unsupported
+
+  where
+    instructionsIO  = case input of
+                        (File filePath) -> readFile filePath
+                        Stdin           -> getContents
+    filePath        = case input of
+                        (File filePath) -> filePath
+                        Stdin           -> "stdin"
+    outfile         = case input of
+                        (File filePath) -> takeBaseName filePath
+                        Stdin           -> "a.out"
+
 
 compileArgsParser :: Parser CompileArgs
 compileArgsParser = CompileArgs
@@ -65,8 +82,22 @@ compileArgsParser = CompileArgs
       metavar "FILE"
     )
   )
-  <*> argument str (
+  <*> (
+    stdinParser <|> fileParser
+  )
+  <**> helper
+
+fileParser :: Parser InputArgs
+fileParser = File <$>
+  argument str (
     metavar "FILE" <>
     help "brainfuck source code" <>
-    action "file")
-  <**> helper
+    action "file"
+  )
+
+stdinParser :: Parser InputArgs
+stdinParser = subparser (
+    command "-" $
+    info (pure Stdin) $
+    progDesc "Read instructions from Stdin"
+  )
