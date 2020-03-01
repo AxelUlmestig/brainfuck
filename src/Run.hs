3660ed1e0fb0cc6fa89317753a2d7da068e43e8b
Axel Ulmestig <axel.ulmestig@gmail.com>
2020-03-01 16:56:48 +0100

    Add stdin option to interpreter

--- src/Run.hs
+++ src/Run.hs
@@ -4,14 +4,15 @@ module Run (
   runArgsParser
 ) where
 
-import           Prelude              hiding (interact, readFile)
+import           Prelude              hiding (getContents, interact, readFile)
 
-import           Control.Applicative  ((<**>))
+import           Control.Applicative  ((<**>), (<|>))
 import           Data.Semigroup       ((<>))
-import           Data.Text.IO         (readFile)
-import           Options.Applicative  (Parser, action, argument, auto,
-                                       completeWith, help, helper, long,
-                                       metavar, option, str, value)
+import           Data.Text.IO         (getContents, readFile)
+import           Options.Applicative  (Parser, action, argument, auto, command,
+                                       completeWith, help, helper, info, long,
+                                       metavar, option, progDesc, str,
+                                       subparser, value)
 import           Text.Parsec          (runP)
 
 import           Brainfuck            (Brainfuck)
@@ -22,8 +23,13 @@ import           Optimizations        (OptimizationLevel (All), optimize)
 data RunArgs = RunArgs
   {
     optimizations :: OptimizationLevel,
-    file          :: String
+    input         :: InputArgs
   }
+  deriving (Show, Read)
+
+data InputArgs =
+    File String |
+    Stdin
   deriving (Show, Read)
 
 runArgsParser :: Parser RunArgs
@@ -34,14 +40,33 @@ runArgsParser = RunArgs
     metavar "LEVEL" <>
     help "all | none, default: all" <>
     completeWith ["all", "none"])
-  <*> argument str (
-    metavar "FILE" <>
-    help "brainfuck source code" <>
-    action "file")
+  <*> (
+    stdinParser <|> fileParser
+  )
   <**> helper
 
+fileParser :: Parser InputArgs
+fileParser = File <$>
+  argument str (
+    metavar "FILE" <>
+    help "brainfuck source code" <>
+    action "file"
+  )
+
+stdinParser :: Parser InputArgs
+stdinParser = subparser (
+    command "-" $
+    info (pure Stdin) $
+    progDesc "Read instructions from Stdin"
+  )
+
 run :: RunArgs -> IO ()
-run (RunArgs optLevel filePath) = do
+run (RunArgs optLevel Stdin) = do
+  instructions <- getContents
+  case runP pBrainfuck 0 "stdin" instructions of
+    Left err  -> print err
+    Right ops -> interact $ optimize optLevel ops
+run (RunArgs optLevel (File filePath)) = do
   instructions <- readFile filePath
   case runP pBrainfuck 0 filePath instructions of
     Left err  -> print err
